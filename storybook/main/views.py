ed483536739c84c426e76857dfd0f6bdf7f73f94
TMZSeif <seiftamer.faresnegm@gmail.com>
2022-08-03 00:55:34 +0300

    Added Home Page

--- storybook/main/views.py
+++ storybook/main/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Storybook</h1>")
+def home(request):
+    return render(request, "main/home.html", {})
